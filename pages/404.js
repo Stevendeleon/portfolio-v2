25a6a345f0674d663c45d82920ff3982b57ffbc3
Steven DeLeon <sdells28@gmail.com>
2021-03-22 11:49:55 -0400

    Added custom 404Page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+function NotFoundPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-center">
+      <h1 className="mb-20 text-2xl font-light sm:text-4xl md:text-6xl">
+        <span className="font-medium">404:</span> Page Not Found
+      </h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
