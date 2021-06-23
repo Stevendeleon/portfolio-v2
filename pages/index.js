aa590c4d37c2228bae66811278c1e798ecf9cb82
Steven DeLeon <steven@Stevens-Air.home>
2021-06-23 09:40:02 -0400

    Resume Update for NC

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
         <h1 className="mt-24 text-4xl font-bold md:text-6xl">Steven DeLeon</h1>
         <p className="mt-3 text-gray-600 text-md md:text-2xl dark:text-green-500">
-          Web Developer | Graphic Designer | <span className="">NY</span>
+          Web Developer | Graphic Designer | <span className="">NY &rarr; NC</span>
         </p>
       </div>
     </Layout>
