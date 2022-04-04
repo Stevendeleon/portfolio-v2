e8e03a74ae4d1a895c152ee8d53efed9f61321dd
Steven DeLeon <me@stevendeleon.com>
2022-04-04 15:22:30 -0400

    removed testing dependecies + files, added img dir

--- pages/__tests__/index.spec.js
+++ pages/__tests__/index.spec.js
@@ -1,13 +1,9 @@
-import Home from "../index";
-import { shallow } from "enzyme";
-import { MY_NAME } from "../../constants";
-import React from "react";
-
-// TODO: get enzyme + jest to work together
-xdescribe("home page - unit test", () => {
-  xit("should render the home page", () => {
-    const wrapper = shallow(<Home />);
-    console.log(wrapper)
-    // expect(wrapper.find('[data-test-id="my-name"]').text()).toEqual(MY_NAME);
-  });
-});
+// const Home = require("../index");
+//
+// describe("home page - unit test", () => {
+//   it("should render the home page", () => {
+//
+//     // expect to find the home page
+//     expect(Home.isLoaded()).toBe(true);r
+//   });
+// });
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Layout from "./../components/layout";
-import { JOB_TITLE, MY_NAME} from "../constants";
+import { JOB_TITLE, MY_NAME } from "../constants";
 
 export default function Home() {
   return (
     <Layout>
       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
-        <h1 className="mt-24 text-4xl font-bold md:text-6xl" data-test-id="my-name">{MY_NAME}</h1>
+        <h1 className="mt-24 text-4xl font-bold md:text-6xl" data-test-id="my-name">{ MY_NAME }</h1>
         <p className="mt-3 text-gray-600 text-md md:text-2xl dark:text-green-500" data-test-id="job-title">
-          {JOB_TITLE}
+          { JOB_TITLE }
         </p>
       </div>
     </Layout>
