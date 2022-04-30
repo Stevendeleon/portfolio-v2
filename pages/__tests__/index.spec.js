90bd773730054ce5a3051e02d30adf1377064076
Steven DeLeon <me@stevendeleon.com>
2022-04-30 17:45:06 -0400

    tests

--- pages/__tests__/index.spec.js
+++ pages/__tests__/index.spec.js
@@ -1,9 +1,15 @@
-// const Home = require("../index");
-//
-// describe("home page - unit test", () => {
-//   it("should render the home page", () => {
-//
-//     // expect to find the home page
-//     expect(Home.isLoaded()).toBe(true);r
-//   });
-// });
+import React from 'react'
+import { render } from '@testing-library/react'
+import Home from '../index'
+import { MY_NAME } from "../../constants";
+
+xdescribe("Home page - Unit Test", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = render(<Home />)
+  })
+  xit("should render the home page", () => {
+   // expect the [data-test-id="my-name"] to match the value from constants/index.js
+    expect(wrapper.find('[data-test-id="my-name"]').text()).toBe(MY_NAME)
+  })
+})
